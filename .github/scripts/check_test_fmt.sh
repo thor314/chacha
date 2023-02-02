abe82e7bac4d069fbfecb498b20c3ed278aa8126
Thor Kamphefner <thorck@pm.me>
2023-02-01 17:21:36 -0800

    nightly compiler in .github

--- .github/scripts/check_test_fmt.sh
+++ .github/scripts/check_test_fmt.sh
@@ -11,6 +11,7 @@ set -euo pipefail # exit immediately if command ends with non-zero exit status.
 # rustup component add rustfmt
 
 # lint test fmt 
+rustup default nightly
 cargo clippy -- -Dwarnings
 cargo test --all-features --verbose
 cargo fmt --all -- --check
